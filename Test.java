4d6f257800ed656f8495d164f6d882d7b035d872
ShZgZ <s.shogo.z89@gmail.com>
2016-02-21 01:00:48 +0900

    My first commit(Add HelloWorld code by Java)

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Test{
+
+        public static void main(String[] args) throws IOException{
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Hello World");
+        }
+}
